2db77c9583c5751964091eb43ad132366c4fa3a0
gilax <gilad.wolf@mail.huji.ac.il>
2016-06-09 23:20:02 +0300

    yguyg

--- check.py
+++ check.py
@@ -1,79 +1,70 @@
 __author__ = 'gilax'
 
 MAX = 4
-PROMISS_PERCENT = 95
 
 from load_tweets import *
 from collections import Counter
 import numpy as np
 
-X,y = load_dataset()
 
-labels = [[0, 1, 2, 3], [4, 5, 6, 7, 8, 9]]
-
-all_tweets_list = ["" for _ in range(len(labels))]
-all_tweets = ""
-# count all the words in all the tweets
-for i in range(len(X)):
-    tweet = X[i].lower()
-    index = y[i]
-    if index in labels[0]:
-        all_tweets_list[0] += " " + tweet
-    else:
-        all_tweets_list[1] += " " + tweet
-    all_tweets += " " + tweet
-
-list_per_group = []
-
-for label in range(len(labels)):
-    counter = Counter(all_tweets_list[label].split()).most_common()
-    list_per_group.append(counter)
-
-# #get a list of all the Prepostion in the english language
-# get_Prepostion_List = [line.rstrip('\n') for line in open('PrepositionsList')]
-
-# list of dictionaries for every person with word as key and amount in tweets as value
-dictionary_per_group = [{k: v for k, v in list_per_group[i]}
-                                        for i in range(len(list_per_group))]
-
-check = open('check.txt', 'w')
-
-names = pandas.read_csv("names.txt", header=None)
-namesIndex, names = names[0], names[1]
-
-# count all the words in all the tweets
-counter = Counter(all_tweets.split()).most_common()
-all_counter = []
-
-# print(counter)
-count0 = 0
-count1 = 0
-for word, amount in counter:
-    to_write = [0,0]
-
-    for i in range(len(dictionary_per_group)):
-        if word in dictionary_per_group[i].keys():
-            percent = round(dictionary_per_group[i][word] / amount * 100, 2)
-            percent = dictionary_per_group[i][word]
-            to_write.append(percent)
-            # if i in labels[0]:
-            #     to_write[0] += percent
-            # else:
-            #     to_write[1] += percent
-
-       # else:
-            #to_write.append(0)
-    to_write[0] = round(to_write[0])
-    to_write[1] = round(to_write[1])
-
-    if to_write[0] >= PROMISS_PERCENT or to_write[1] >= PROMISS_PERCENT:
-        if to_write[0] >= PROMISS_PERCENT:
-            count0 += 1
+def get_common_words(X, y, labels, promise):
+    """
+    returns the common words and the percentage of them in each group
+    :param X: tweets
+    :param y: names indexes
+    :param labels: list of two groups of names indexes
+    :param promise: percent that promising the separate
+    :return: a list of [word, [percent in first group, percent in second group]]
+    """
+    all_tweets_list = ["" for _ in range(len(labels))]
+    all_tweets = ""
+    # count all the words in all the tweets
+    for i in range(len(X)):
+        tweet = X[i].lower()
+        if y[i] in labels[0]:
+            index = 0
         else:
-            count1+=1
-        check.write(word + to_write.__str__() + "\n")
-        all_counter.append((word, to_write))
-    # check.write(names[i] + "\n")
-    # for string in my_dict[i]:
-    #     check.write(string.__str__() + "\n")
-    # check.write("\n\n")
+            index = 1
+        all_tweets_list[index] += " " + tweet
+        all_tweets += " " + tweet
+
+    list_per_person = []
+
+    for label in range(len(labels)):
+        counter = Counter(all_tweets_list[label].split()).most_common()
+        list_per_person.append(counter)
+
+    # #get a list of all the Prepostion in the english language
+    # get_Prepostion_List = [line.rstrip('\n') for line in open('PrepositionsList')]
+
+    # list of dictionaries for every person with word as key and amount in tweets as value
+    dictionary_per_person = [{k: v for k, v in list_per_person[i]}
+                                            for i in range(len(list_per_person))]
+
+    check = open('check.txt', 'w')
+
+    # count all the words in all the tweets
+    counter = Counter(all_tweets.split()).most_common()
+    all_counter = []
+
+    for word, amount in counter:
+        to_write = []
+        for i in range(len(dictionary_per_person)):
+            if word in dictionary_per_person[i].keys():
+                percent = round(dictionary_per_person[i][word] / amount * 100, 2)
+                to_write.append(percent)
+            else:
+                to_write.append(0)
+        if any(to_write[i] >= promise and dictionary_per_person[i][word] > MAX
+               for i in range(len(dictionary_per_person))):
+            check.write(word + to_write.__str__() + "\n")
+            all_counter.append((word, to_write))
+
+    return all_counter
+
+
+tweets, names = load_dataset()
+
+politician_labels = [[i for i in range(4)], [i + 4 for i in range(6)]]
+
+print(len(get_common_words(tweets, names, politician_labels, 95)))
