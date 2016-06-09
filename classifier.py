afea98fe3d27db1a32cd8b784cfe5cc0de866155
gilax <gilad.wolf@mail.huji.ac.il>
2016-06-09 16:49:54 +0300

    yguyg

--- classifier.py
+++ classifier.py
@@ -27,8 +27,7 @@ class Classifier(object):
         :param X: A list of length m containing the tweet's texts (strings)
         :return: y_hat - a vector of length m that contains integers between 0 - 9
         """
-
-    raise NotImplementedError("TODO: Implement this method by 12pm tomorrow!")
+        # TODO implement
 
     def word_count(self, tweets, name):
         """
