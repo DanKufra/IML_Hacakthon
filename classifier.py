c7a73fcaa6100aa8e3a86c3b396430c5d11d8beb
dan_kufra <dankufra@gmail.com>
2016-06-09 15:21:56 +0300

    y

---
+++ classifier.py
@@ -0,0 +1,21 @@
+"""
+===================================================
+     Introduction to Machine Learning (67577)
+             IML HACKATHON, June 2016
+
+            **  Tweets Classifier  **
+
+Auther(s):
+
+===================================================
+"""
+
+class Classifier(object):
+
+    def classify(self,X):
+        """
+        Recieves a list of m unclassified tweets, and predicts for each one which celebrity posted it.
+        :param X: A list of length m containing the tweet's texts (strings)
+        :return: y_hat - a vector of length m that contains integers between 0 - 9
+        """
+    raise NotImplementedError("TODO: Implement this method by 12pm tomorrow!")
